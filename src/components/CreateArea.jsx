53010b3ab8995f4cf531a64b1d1f446b5aa36d0c
EmmaOkafor <okaforifebuche26@gmail.com>
2023-09-09 14:20:38 +0100

    Modified the create area

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import AddIcon from '@mui/icons-material/Add';
-// import Fab from '@mui/material/Fab';
-import Zoom from '@mui/material/Zoom';
-import { Fab } from '@mui/material';
+import AddIcon from "@mui/icons-material/Add";
+import Zoom from "@mui/material/Zoom";
+import { Fab } from "@mui/material";
 
 function CreateArea(props) {
   const [note, setNote] = useState({
@@ -11,6 +10,8 @@ function CreateArea(props) {
   });
 
   const [isExpanded, setIsExpanded] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
   function handleChange(event) {
     const { name, value } = event.target;
     setNote((prevNote) => {
@@ -19,46 +20,58 @@ function CreateArea(props) {
         [name]: value
       };
     });
+
+    // Clear error message when the user starts typing
+    setErrorMessage("");
   }
 
   function submitNote(event) {
-    props.onAdd(note);
-    setNote({
-      title: "",
-      content: ""
-    });
+    // Check if both title and content are not empty
+    if (note.title.trim() !== "" && note.content.trim() !== "") {
+      props.onAdd(note);
+      setNote({
+        title: "",
+        content: ""
+      });
+    } else {
+      // Display a single error message for both fields
+      setErrorMessage("All fields are required, kindly fill them");
+    }
     event.preventDefault();
   }
 
-  function expand(){
+  function expand() {
     setIsExpanded(true);
   }
 
   return (
-    <div>
+    <div className="create-area">
+    {errorMessage && <p className="error-message">{errorMessage}</p>}
       <form className="create-note">
-        {isExpanded ? <input
-          onChange={handleChange}
-          name="title"
-          placeholder="Title"
-          value={note.title}
-        /> : null}
-
-<div>
-      
-    </div>
+        {isExpanded ? (
+          <div>
+            <input
+              onChange={handleChange}
+              name="title"
+              placeholder="Title"
+              value={note.title}
+            />
+          </div>
+        ) : null}
         <textarea
           onChange={handleChange}
           name="content"
-
-          onClick ={expand}
+          onClick={expand}
           placeholder="Take a note..."
-          rows={isExpanded? "3" : "1"}
+          rows={isExpanded ? "3" : "1"}
           value={note.content}
         />
-        <Zoom in ={isExpanded? true : false}>
-        <Fab onClick={submitNote}><AddIcon /> </Fab>
+        <Zoom in={isExpanded ? true : false}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
         </Zoom>
+        
       </form>
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -7,10 +7,12 @@ function Note(props) {
   }
 
   return (
-    <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-      <button onClick={handleClick}><DeleteIcon /> </button>
+    <div className="note-container">
+      <div className="note">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+        <button onClick={handleClick}><DeleteIcon /> </button>
+      </div>
     </div>
   );
 }
